6a0de2f8e610fb4224041ae6e5bf6004cef79270
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-08 15:20:38 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,6 @@
+//Task 1: Calculate the Tip
+let bill= 129; //Bill example
+let tip= bill >= 100 ? bill* 0.18 : bill* 0.15; //If Bill is larger than 
+
+console.log(tip);//Output: Tip amount(bill* tip%)
+
